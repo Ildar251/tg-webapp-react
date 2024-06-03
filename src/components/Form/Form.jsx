e975ed9f893faf839a9385692d733645a9ca0d86
Ildar251 <rakhmatullin-ildar@mail.ru>
2024-06-03 08:52:33 +0300

    Router

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,153 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import "./Form.css";
+import axios from 'axios';
+import { useForm } from "react-hook-form";
+import InputMask from 'react-input-mask';
+import { useTelegram } from "../../hooks/useTelegram";
+
+const Form = () => {
+    const [suggestions, setSuggestions] = useState([]);
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+
+    const { tg } = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            phone,
+            address
+        }
+        console.log("Sending data:", data); // Добавьте этот лог для отладки
+        tg.sendData(JSON.stringify(data))
+    }, [phone, address])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
+
+
+    const {
+        register,
+        formState: { errors, isValid, },
+        handleSubmit,
+        setValue,
+    } = useForm({
+        mode: 'onBlur'
+    });
+
+
+    useEffect(() => {
+        tg.ready();
+
+        tg.MainButton.show();
+        if (!isValid) {
+            tg.MainButton.disable();
+        } else {
+            tg.MainButton.enable();
+        }
+
+        tg.MainButton.setParams({
+            text: "Отправить данные"
+        })
+    })
+
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data))
+    }
+    const handlePhoneChange = async (e) => {
+        const value = e.target.value;
+        setPhone(value);
+    }
+
+    const handleAddressChange = async (e) => {
+        const value = e.target.value;
+        setAddress(value);
+
+        if (value.length > 3) {
+            try {
+                const response = await axios.post(
+                    'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address',
+                    { query: value },
+                    {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json',
+                            'Authorization': `Token ${process.env.REACT_APP_DADATE_API_KEY}`
+                        }
+                    }
+                );
+                setSuggestions(response.data.suggestions);
+            } catch (error) {
+                console.error('Error fetching address suggestions:', error);
+            }
+        } else {
+            setSuggestions([]);
+        }
+    };
+
+    const handleSuggestionClick = (suggestion) => {
+        setValue('address', suggestion.value);
+        setAddress(suggestion.value);
+        setSuggestions([]);
+    };
+
+
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h1>Форма закакза</h1>
+
+            <label>
+                <span>Ваш номер</span>
+                <InputMask
+                    mask="+7 (999) 999-99-99"
+
+                    {...register('phone', {
+                        required: "Это поле обязательно для заполнения",
+                        pattern: {
+                            value: /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/,
+                            message: "Введите корректный номер телефона"
+                        }
+                    })}
+
+                    value={phone}
+                    onChange={handlePhoneChange}
+                >
+                    {(inputProps) => <input {...inputProps} type="tel" />}
+                </InputMask>
+            </label>
+            <div>{errors?.phone && <span>{errors?.phone?.message || "Ошибка"}</span>}</div>
+
+
+            <label>
+                <span>Ваш адрес</span>
+                <input
+                    {...register('address', {
+                        required: "Это поле обязательно для заполнения"
+                    })}
+                    value={address}
+                    onChange={handleAddressChange}
+                />
+                {suggestions.length > 0 && (
+                    <ul className="suggestions-list">
+                        {suggestions.map((suggestion, index) => (
+                            <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                                {suggestion.value}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </label>
+            <div>{errors?.address && <span>{errors?.address.message || "Ошибка"}</span>}</div>
+
+            {/* <input type='submit' disabled={!isValid} /> */}
+        </form>
+    );
+};
+
+export default Form;
---
+++ src/components/Manager/Manager.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Manager';
+
+const Manager = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Manager;
