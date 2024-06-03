69aed75212ecfbb7d0e307990d594c518120c122
Ildar251 <rakhmatullin-ildar@mail.ru>
2024-06-03 11:51:09 +0300

    css

--- src/components/Manager/Manager.jsx
+++ src/components/Manager/Manager.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
+import '../../App.css';
 import "./Manager.css";
 const Manager = () => {
     const [orders, setOrders] = useState([]);
