f3acb9f643dc9d3c2ea595f69a903ad63da922dc
Ildar251 <rakhmatullin-ildar@mail.ru>
2024-06-03 17:27:59 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import { Route, Routes } from "react-router-dom";
 import './App.css';
 import Manager from './components/Manager/Manager';
 import Form from './components/Form/Form';
+import { Router } from 'express';
 
 
 
 function App() {
-  
-  
+
+
 
   return (
-    <div className="App">
+    <Router basename="/tg-webapp-react">
+      <div className="App">
         <Routes>
           <Route index element={<Form />} />
           <Route path={'manager'} element={<Manager />} />
         </Routes>
-    </div>
+      </div>
+    </Router>
+
   );
 }
 
